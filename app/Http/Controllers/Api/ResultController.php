81e30472ac1f43d0d919bc67861ae2dfca041c2f
Stas <postnikov.sa@ya.ru>
2024-01-13 00:09:32 +0700

    create api docs, update api EP

--- app/Http/Controllers/Api/ResultController.php
+++ app/Http/Controllers/Api/ResultController.php
@@ -6,28 +6,75 @@ use App\Http\Controllers\Controller;
 use App\Models\Member;
 use App\Models\Result;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Validation\ValidationException;
 
 class ResultController extends Controller
 {
+    /**
+     * @OA\Post(
+     *     path="/api/save-result",
+     *     tags={"Results"},
+     *     summary="Сохранение результата для участника",
+     *     description="Сохраняет результат для участника игры.",
+     *     @OA\RequestBody(
+     *         required=true,
+     *         @OA\JsonContent(
+     *             type="object",
+     *             @OA\Property(property="email", type="string", example="example@email.com", description="Email участника"),
+     *             @OA\Property(property="milliseconds", type="integer", example=5000, description="Время за которое пользователь прошел игру")
+     *         )
+     *     ),
+     *     @OA\Response(
+     *         response="200",
+     *         description="Результат успешно сохранен",
+     *         @OA\JsonContent(
+     *             type="object",
+     *             @OA\Property(property="message", type="string", example="Результат успешно сохранен")
+     *         )
+     *     ),
+     *     @OA\Response(
+     *         response="422",
+     *         description="Ошибка валидации",
+     *         @OA\JsonContent(
+     *             type="object",
+     *             @OA\Property(property="error", type="object", description="Сообщения об ошибках валидации")
+     *         )
+     *     ),
+     *     @OA\Response(
+     *         response="500",
+     *         description="Произошла ошибка при сохранении результата",
+     *         @OA\JsonContent(
+     *             type="object",
+     *             @OA\Property(property="error", type="string", example="Произошла ошибка при сохранении результата")
+     *         )
+     *     )
+     * )
+     */
     public function saveResult(Request $request)
     {
         try {
             $request->validate([
+                'email' => 'email|nullable',
                 'milliseconds' => 'required|integer',
             ]);
 
             $member = null;
-            if ($request->has('email')) {
-                $member = Member::firstOrCreate(['email' => $request->input('email')]);
+
+            if ($request->filled('email')) {
+
+                $member = Member::firstOrNew(['email' => $request->input('email')]);
+
+                if (!$member->exists) {
+                    $member->save();
+                }
             }
 
-            Result::create([
-                'member_id' => $member ? $member->id : null,
-                'milliseconds' => $request->input('milliseconds'),
-            ]);
+            Result::updateOrCreate(
+                ['member_id' => $member ? $member->id : null],
+                ['milliseconds' => $request->input('milliseconds')]
+            );
 
-            // Ответ
             return response()->json(['message' => 'Результат успешно сохранен']);
         } catch (ValidationException $e) {
             return response()->json(['error' => $e->validator->errors()], 422);
--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -7,6 +7,20 @@ use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Routing\Controller as BaseController;
 
+/**
+ * @OA\Info(
+ *     version="1.0.0",
+ *     title="Player API",
+ *     description="API для работы с с проектом",
+ *     @OA\Contact(
+ *         email="postnikov.sa@ya.ru"
+ *     ),
+ *     @OA\License(
+ *         name="Apache 2.0",
+ *         url="http://www.apache.org/licenses/LICENSE-2.0.html"
+ *     )
+ * )
+ */
 class Controller extends BaseController
 {
     use AuthorizesRequests;
