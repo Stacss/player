578d2277b5c85cf022bf7d9328216536c9aaaf62
Stas <postnikov.sa@ya.ru>
2024-01-13 16:55:29 +0700

    make seeds

---
+++ database/factories/ResultFactory.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Member;
+use App\Models\Result;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Result>
+ */
+class ResultFactory extends Factory
+{
+    protected $model = Result::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition()
+    {
+        return [
+            'member_id' => Member::factory(),
+            'milliseconds' => $this->faker->numberBetween(1, 10000),
+        ];
+    }
+}
