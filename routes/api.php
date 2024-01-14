3b64d006e49c760c9d54520474f817e3dc1d1331
Stas <postnikov.sa@ya.ru>
2024-01-15 00:17:57 +0700

    Add swagger

--- routes/api.php
+++ routes/api.php
@@ -19,3 +19,4 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 Route::post('/save-result', [ResultController::class, 'saveResult']);
+Route::get('/top-results', [ResultController::class, 'getTopResults']);
